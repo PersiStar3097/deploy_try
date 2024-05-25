2d86a9a9924c3533b28b4c78ff352cceeafea377
PersiStar3097 <170253340+PersiStar3097@users.noreply.github.com>
2024-05-25 19:58:14 +0900

    g

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/deploy_try/' : './',
-  base: process.env.GITHUB_PAGES  // この行を追加
-        ? "/deploy_try/"            // この行を追加
-        : "./",
+  base: process.env.NODE_ENV === 'production' ? '/docs/' : './',
+  // base: process.env.GITHUB_PAGES  // この行を追加
+  //       ? "/deploy_try/"            // この行を追加
+  //       : "./",
   assetsDir:'./',
   plugins: [
     vue(),
