e664ece4289588f4ae6f2fb21bbc83b380ba4463
PersiStar3097 <170253340+PersiStar3097@users.noreply.github.com>
2024-05-25 20:47:18 +0900

    fsadafda

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 
-const path = require('path')
+import {fileURLToPath, URL} from 'node:url'
+
+// const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.NODE_ENV === 'production' ? '/docs/' : './',
-  pulciPath: '/deploy_try/',
+  base: '/deploy_try/',
   // base: process.env.GITHUB_PAGES  // この行を追加
   //       ? "/deploy_try/"            // この行を追加
   //       : "./",
-  assetsDir:'./',
+  // assetsDir:'./',
   plugins: [
     vue(),
     // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
@@ -19,10 +21,11 @@ export default defineConfig({
       autoImport: true,
     }),
   ],
-  define: { 'process.env': {} },
+  // define: { 'process.env': {} },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      // '@': path.resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
   build: {
